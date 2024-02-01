05b67afa4167de7b3d0abf90e9a6ce22949191a6
Mark Koberlein <mkoberlein@gmail.com>
2024-02-01 00:27:17 -0500

    added session manager plugin install to aws

--- aws.sh
+++ aws.sh
@@ -21,3 +21,8 @@ cd ~
 wget https://dy9cqqaswpltd.cloudfront.net/workbenchDDBLocal-linux.run
 sudo ./workbenchDDBLocal-linux.run
 mv workbenchDDBLocal-linux.run Trash
+
+cd ~
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+sudo dpkg -i session-manager-plugin.deb
+
