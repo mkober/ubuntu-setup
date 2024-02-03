eb85bd3f1f1612f518cccbe31630ff35f2c20b08
Mark Koberlein <mkoberlein@gmail.com>
2024-02-03 11:53:32 -0500

    added post install commands for docker user/group

--- docker.sh
+++ docker.sh
@@ -16,3 +16,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Post install to setup docker user/group to run docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
