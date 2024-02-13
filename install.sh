2a9a89af5b6bb4eaa0fd0c9a5bdc35f952455f24
Mark Koberlein <mkoberlein@gmail.com>
2024-02-12 22:11:57 -0500

    added install gnome-shell-extensions

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
 sudo apt update
-sudo apt install -y ntpdate ansible libfuse2 git git-lfs gh vim tmux gnome-tweaks build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf ninja-build gettext cmake unzip curl fd-find ripgrep ninja-build gettext cmake unzip curl python3-neovim python3.10-venv python3-pip python3-poetry
+sudo apt install -y gnome-shell-extensions ntpdate ansible libfuse2 git git-lfs gh vim tmux gnome-tweaks build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf ninja-build gettext cmake unzip curl fd-find ripgrep ninja-build gettext cmake unzip curl python3-neovim python3.10-venv python3-pip python3-poetry
 
 mkdir ~/Repos
 mkdir ~/Trash
