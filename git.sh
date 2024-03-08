e219045c28b97c8da46f8709e4602df9305ff9bd
Mark Koberlein <mkoberlein@gmail.com>
2024-03-07 20:20:48 -0500

    added credential-helper to git global config

--- git.sh
+++ git.sh
@@ -6,3 +6,5 @@ git config --global user.name "Mark Koberlein"
 git config --global user.email "mkoberlein@gmail.com"
 git config --global init.defaultBranch main
 git config --global core.editor "nvim"
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
